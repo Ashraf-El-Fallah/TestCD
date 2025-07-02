f93ed9247b59175decf33562ebbda93dff906a19
Ashraf-El-Fallah <elfallahashraf44@gmail.com>
2025-07-02 18:04:19 +0300

    change name

--- app/src/main/java/com/example/testcd/MainActivity.kt
+++ app/src/main/java/com/example/testcd/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TestCDTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Sayed Ahmed",
+                        name = "Sayed",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
